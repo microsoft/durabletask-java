af39dfd7e44080c7f1a78386c1c2f1d1e0f70a17
Chris Gillum <cgillum@microsoft.com>
2021-07-10 12:23:21 -0700

    Convert to multi-project structure

--- samples/src/main/java/io/durabletask/samples/FanOutFanInPattern.java
+++ samples/src/main/java/io/durabletask/samples/FanOutFanInPattern.java
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-package com.microsoft.durabletask.samples;
+package io.durabletask.samples;
 
 import com.microsoft.durabletask.*;
 
