759d05685d209cbbf699afc38088df2dc009d076
kaibocai <89094811+kaibocai@users.noreply.github.com>
2022-08-03 10:31:28 -0500

    udpate java doc for the method fix

--- client/src/main/java/com/microsoft/durabletask/OrchestrationMetadata.java
+++ client/src/main/java/com/microsoft/durabletask/OrchestrationMetadata.java
@@ -126,7 +126,7 @@ public final class OrchestrationMetadata {
     /**
      * Gets a value indicating whether the orchestration instance was running at the time this object was fetched.
      *
-     * @return {@code true} if the orchestration was in a running state; otherwise {@code false}
+     * @return {@code true} if the orchestration existed and was in a running state; otherwise {@code false}
      */
     public boolean isRunning() {
         return isInstanceFound() && this.runtimeStatus == OrchestrationRuntimeStatus.RUNNING;
