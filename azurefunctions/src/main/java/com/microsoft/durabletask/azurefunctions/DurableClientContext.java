84e313eef71629a359778b0884125b812a9fb15a
Shreyas Gopalakrishna <shreyasg@microsoft.com>
2023-01-24 13:07:21 -0600

    Added createHttpManagementPayload API

--- azurefunctions/src/main/java/com/microsoft/durabletask/azurefunctions/DurableClientContext.java
+++ azurefunctions/src/main/java/com/microsoft/durabletask/azurefunctions/DurableClientContext.java
@@ -60,36 +60,53 @@ public class DurableClientContext {
         //       request headers into consideration and generate the base URL accordingly.
         //       More info: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Forwarded.
         //       One potential workaround is to set ASPNETCORE_FORWARDEDHEADERS_ENABLED to true.
+
+        // Construct the response as an HTTP 202 with a JSON object payload
+        return request.createResponseBuilder(HttpStatus.ACCEPTED)
+                .header("Location", this.getInstanceStatusURL(request, instanceId) + "?" + this.requiredQueryStringParameters)
+                .header("Content-Type", "application/json")
+                .body(this.getClientResponseLinks(request, instanceId))
+                .build();
+    }
+
+    /**
+     * Creates a {@link HttpManagementPayload} that is useful for checking the status of the specified instance.
+     *
+     * @param request    The HTTP request that triggered the current orchestration instance.
+     * @param instanceId The ID of the orchestration instance to check.
+     * @return The {@link HttpManagementPayload} with URLs that can be used to query the status of the orchestration,
+     *         raise events to the orchestration, or terminate the orchestration.
+     */
+    public HttpManagementPayload createHttpManagementPayload(HttpRequestMessage<?> request, String instanceId) {
+        return this.getClientResponseLinks(request, instanceId);
+    }
+
+    private HttpManagementPayload getClientResponseLinks(HttpRequestMessage<?> request, String instanceId) {
+        String instanceStatusURL = this.getInstanceStatusURL(request, instanceId);
+        return new HttpManagementPayload(instanceId, instanceStatusURL, this.requiredQueryStringParameters);
+    }
+
+    private String getInstanceStatusURL(HttpRequestMessage<?> request, String instanceId) {
         String baseUrl = request.getUri().getScheme() + "://" + request.getUri().getAuthority();
         String encodedInstanceId;
+
         try {
             encodedInstanceId = URLEncoder.encode(instanceId, StandardCharsets.UTF_8.toString());
         } catch (UnsupportedEncodingException ex) {
             throw new IllegalArgumentException("Failed to encode the instance ID: " + instanceId, ex);
         }
 
-        String instanceStatusURL = baseUrl + "/runtime/webhooks/durabletask/instances/" + encodedInstanceId;
+        return baseUrl + "/runtime/webhooks/durabletask/instances/" + encodedInstanceId;
+    }
 
-        // Construct the response as an HTTP 202 with a JSON object payload
-        return request.createResponseBuilder(HttpStatus.ACCEPTED)
-                .header("Location", instanceStatusURL + "?" + this.requiredQueryStringParameters)
-                .header("Content-Type", "application/json")
-                .body(new HttpCreateCheckStatusResponse(
-                        instanceId,
-                        instanceStatusURL,
-                        this.requiredQueryStringParameters))
-                .build();
-            }
+    public static class HttpManagementPayload {
+        private final String id;
+        private final String purgeHistoryDeleteUri;
+        private final String sendEventPostUri;
+        private final String statusQueryGetUri;
+        private final String terminatePostUri;
 
-    private static class HttpCreateCheckStatusResponse {
-        // These fields are serialized to JSON
-        public final String id;
-        public final String purgeHistoryDeleteUri;
-        public final String sendEventPostUri;
-        public final String statusQueryGetUri;
-        public final String terminatePostUri;
-
-        public HttpCreateCheckStatusResponse(
+        public HttpManagementPayload(
                 String instanceId,
                 String instanceStatusURL,
                 String requiredQueryStringParameters) {
@@ -98,6 +115,51 @@ public class DurableClientContext {
             this.sendEventPostUri = instanceStatusURL + "/raiseEvent/{eventName}?" + requiredQueryStringParameters;
             this.statusQueryGetUri = instanceStatusURL + "?" + requiredQueryStringParameters;
             this.terminatePostUri = instanceStatusURL + "/terminate?reason={text}&" + requiredQueryStringParameters;
+        }
+
+        /**
+         * Gets the ID of the orchestration instance.
+         *
+         * @return The ID of the orchestration instance.
+         */
+        public String getId() {
+            return this.id;
+        }
+
+        /**
+         * Gets the HTTP GET status query endpoint URL.
+         *
+         * @return The HTTP URL for fetching the instance status.
+         */
+        public String getStatusQueryGetUri() {
+            return this.statusQueryGetUri;
+        }
+
+        /**
+         * Gets the HTTP POST external event sending endpoint URL.
+         *
+         * @return The HTTP URL for posting external event notifications.
+         */
+        public String getSendEventPostUri() {
+            return this.sendEventPostUri;
+        }
+
+        /**
+         * Gets the HTTP POST instance termination endpoint.
+         *
+         * @return The HTTP URL for posting instance termination commands.
+         */
+        public String getTerminatePostUri() {
+            return this.terminatePostUri;
+        }
+
+        /**
+         * Gets the HTTP DELETE purge instance history by instance ID endpoint.
+         *
+         * @return The HTTP URL for purging instance history by instance ID.
+         */
+        public String getPurgeHistoryDeleteUri() {
+            return this.purgeHistoryDeleteUri;
         }
     }
 }
