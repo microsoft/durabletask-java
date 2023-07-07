b7a47315f45540c23fd974ab5102effb15a3f0a7
Kirstyn Joy Amperiadis <kamperiadis@microsoft.com>
2023-07-07 11:33:09 -0500

    Add end to end tests

--- samples-azure-functions/src/test/java/com/functions/EndToEndTests.java
+++ samples-azure-functions/src/test/java/com/functions/EndToEndTests.java
@@ -5,6 +5,8 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.post;
@@ -26,14 +28,7 @@ public class EndToEndTests {
         Response response = post(startOrchestrationPath);
         JsonPath jsonPath = response.jsonPath();
         String statusQueryGetUri = jsonPath.get("statusQueryGetUri");
-        String runTimeStatus = null;
-        for (int i = 0; i < 15; i++) {
-            Response statusResponse = get(statusQueryGetUri);
-            runTimeStatus = statusResponse.jsonPath().get("runtimeStatus");
-            if (!"Completed".equals(runTimeStatus)) {
-                Thread.sleep(1000);
-            } else break;
-        }
+        String runTimeStatus = waitForCompletion(statusQueryGetUri);
         assertEquals("Completed", runTimeStatus);
     }
 
@@ -59,4 +54,44 @@ public class EndToEndTests {
         runTimeStatus = statusResponse.jsonPath().get("runtimeStatus");
         assertEquals("Terminated", runTimeStatus);
     }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    public void restart(boolean restartWithNewInstanceId) throws InterruptedException {
+        String startOrchestrationPath = "/api/StartOrchestration";
+        Response response = post(startOrchestrationPath);
+        JsonPath jsonPath = response.jsonPath();
+        String statusQueryGetUri = jsonPath.get("statusQueryGetUri");
+        String runTimeStatus = waitForCompletion(statusQueryGetUri);
+        assertEquals("Completed", runTimeStatus);
+        Response statusResponse = get(statusQueryGetUri);
+        String instanceId = statusResponse.jsonPath().get("instanceId");
+
+        String restartPostUri = jsonPath.get("restartPostUri") + "&restartWithNewInstanceId=" + restartWithNewInstanceId;
+        Response restartResponse = post(restartPostUri);
+        JsonPath restartJsonPath = restartResponse.jsonPath();
+        String restartStatusQueryGetUri = restartJsonPath.get("statusQueryGetUri");
+        String restartRuntimeStatus = waitForCompletion(restartStatusQueryGetUri);
+        assertEquals("Completed", restartRuntimeStatus);
+        Response restartStatusResponse = get(restartStatusQueryGetUri);
+        String newInstanceId = restartStatusResponse.jsonPath().get("instanceId");
+        if (restartWithNewInstanceId) {
+            assertNotEquals(instanceId, newInstanceId);
+        } else {
+            assertEquals(instanceId, newInstanceId);
+        }
+    }
+
+    private String waitForCompletion(String statusQueryGetUri) throws InterruptedException {
+        String runTimeStatus = null;
+        for (int i = 0; i < 15; i++) {
+            Response statusResponse = get(statusQueryGetUri);
+            runTimeStatus = statusResponse.jsonPath().get("runtimeStatus");
+            if (!"Completed".equals(runTimeStatus)) {
+                Thread.sleep(1000);
+            } else break;
+        }
+        return runTimeStatus;
+    }
+
 }
