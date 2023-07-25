bf89164beada518004a49ed14e11d9614485e03d
kaibocai <89094811+kaibocai@users.noreply.github.com>
2023-07-25 17:17:44 -0500

    add e2e test for failure path of retry

--- samples-azure-functions/src/test/java/com/functions/EndToEndTests.java
+++ samples-azure-functions/src/test/java/com/functions/EndToEndTests.java
@@ -57,6 +57,16 @@ public class EndToEndTests {
     }
 
     @Test
+    public void retryTestFail() throws InterruptedException {
+        String startOrchestrationPath = "/api/RetriableOrchestrationFail";
+        Response response = post(startOrchestrationPath);
+        JsonPath jsonPath = response.jsonPath();
+        String statusQueryGetUri = jsonPath.get("statusQueryGetUri");
+        boolean pass = pollingCheck(statusQueryGetUri, "Failed", null, Duration.ofSeconds(10));
+        assertTrue(pass);
+    }
+
+    @Test
     public void continueAsNew() throws InterruptedException {
         String startOrchestrationPath = "api/ContinueAsNew";
         Response response = post(startOrchestrationPath);
