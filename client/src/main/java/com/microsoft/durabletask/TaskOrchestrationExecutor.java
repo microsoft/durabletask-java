95734ffd25269385b8dbccdbc5483ac9fe1f5762
Kirstyn Joy Amperiadis <kamperiadis@microsoft.com>
2023-09-05 16:57:57 -0500

    Create the timer tasks as they complete instead of upfront

--- client/src/main/java/com/microsoft/durabletask/TaskOrchestrationExecutor.java
+++ client/src/main/java/com/microsoft/durabletask/TaskOrchestrationExecutor.java
@@ -586,25 +586,32 @@ final class TaskOrchestrationExecutor {
         }
 
         private Task<Void> createTimer(Instant finalFireAt) {
-            Duration remainingTime = Duration.between(this.currentInstant, finalFireAt);
-            if (remainingTime.compareTo(this.maximumTimerInterval) > 0) {
-                List<CompletableFuture<Void>> timers = new ArrayList<>();
-                Instant currentInstant = this.currentInstant;
-                while (remainingTime.compareTo(this.maximumTimerInterval) > 0) {
-                    Instant nextFireAt = currentInstant.plus(this.maximumTimerInterval);
-                    Task<Void> instantTimer = createInstantTimer(this.sequenceNumber++, nextFireAt);
-                    timers.add(instantTimer.future);
-                    currentInstant = nextFireAt;
-                    remainingTime = Duration.between(currentInstant, finalFireAt);
+            Task<Void> firstTimer = createTimerTask(finalFireAt);
+            CompletableFuture<Void> future = createTimerChain(finalFireAt, firstTimer.future);
+            return new CompletableTask<>(future);
+        }
+
+        private CompletableFuture<Void> createTimerChain(Instant finalFireAt, CompletableFuture<Void> currentFuture) {
+            return currentFuture.thenRun(() -> {
+                if (this.currentInstant.compareTo(finalFireAt) > 0) {
+                    return;
                 }
-                Task<Void> finalTimer = createInstantTimer(this.sequenceNumber++, finalFireAt);
-                timers.add(finalTimer.future);
-                CompletableFuture<Void> longTimer = CompletableFuture.allOf(timers.toArray(new CompletableFuture[timers.size()]));
-                return new CompletableTask<>(longTimer);
+                Task<Void> nextTimer = createTimerTask(finalFireAt);
+
+                createTimerChain(finalFireAt, nextTimer.future);
+            });
+        }
+
+        public Task<Void> createTimerTask(Instant finalFireAt) {
+            Task<Void> nextTimer = null;
+            Duration remainingTime = Duration.between(currentInstant, finalFireAt);
+            if (remainingTime.compareTo(maximumTimerInterval) > 0) {
+                Instant nextFireAt = currentInstant.plus(maximumTimerInterval);
+                nextTimer = createInstantTimer(sequenceNumber++, nextFireAt);
+            } else {
+                nextTimer = createInstantTimer(sequenceNumber++, finalFireAt);
             }
-            else {
-                return createInstantTimer(this.sequenceNumber++, finalFireAt);
-            }
+            return nextTimer;
         }
 
         private Task<Void> createInstantTimer(int id, Instant fireAt) {
